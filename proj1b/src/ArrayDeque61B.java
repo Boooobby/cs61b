bf06de6a49c211f3abf3f232b1f6b983ae9242b8
Boooobby <bibobu1223@gmail.com>
2025-09-15 22:47:41 +0800

    proj1b: completed basic functions

--- proj1b/src/ArrayDeque61B.java
+++ proj1b/src/ArrayDeque61B.java
@@ -1,49 +1,91 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayDeque61B<T> implements Deque61B<T> {
 
+    private T[] arr;
+    private int size, arrSize;
+    private int head, tail;
+
     public ArrayDeque61B() {
-        
+        arr = (T[]) new Object[8];
+        arrSize = 8;
+        size = 0;
+        head = 0;
+        tail = arrSize - 1;
     }
 
     @Override
     public void addFirst(T x) {
-
+        head = Math.floorMod(head - 1, arrSize);
+        arr[head] = x;
+        size++;
     }
 
     @Override
     public void addLast(T x) {
-
+        tail = Math.floorMod(tail + 1, arrSize);
+        arr[tail] = x;
+        size++;
     }
 
     @Override
     public List<T> toList() {
-        return List.of();
+        List<T> returnList = new ArrayList<>();
+        if (this.size == 0) {
+            return returnList;
+        }
+
+        for (int i = head; i != tail; i = Math.floorMod(i + 1, arrSize)) {
+            returnList.add(this.get(i));
+        }
+        returnList.add(this.get(tail));
+        return returnList;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public T removeFirst() {
-        return null;
+        if (this.size == 0) {
+            return null;
+        }
+        T returnItem = this.get(head);
+        head = Math.floorMod(head + 1, arrSize);
+        return returnItem;
     }
 
     @Override
     public T removeLast() {
-        return null;
+        if (this.size == 0) {
+            return null;
+        }
+        T returnItem = this.get(tail);
+        tail = Math.floorMod(tail - 1, arrSize);
+        return returnItem;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index >= arrSize) {
+            return null;
+        } else if (head == 0 && index > tail) {
+            return null;
+        } else if (tail == arrSize - 1 && index < head) {
+            return null;
+        } else if (index > tail && index < head) {
+            return null;
+        } else {
+            return arr[index];
+        }
     }
 
     @Override
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.util.Deque;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -10,14 +11,104 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+    @Test
+    @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+    void noNonTrivialFields() {
+        List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                .toList();
+
+        assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Test the methods add")
+    void testAdd() {
+        Deque61B<Character> d = new ArrayDeque61B<>();
+        d.addFirst('b');
+        d.addFirst('a');
+        d.addLast('c');
+        d.addLast('d');
+        d.addLast('e');
+
+        assertThat(d.toList()).containsExactly('a', 'b', 'c', 'd', 'e').inOrder();
+    }
+
+    @Test
+    @DisplayName("Test the method get")
+    void testGet() {
+        Deque61B<Integer> d = new ArrayDeque61B<>();
+        d.addFirst(5);
+        d.addFirst(4);
+        d.addFirst(3);
+        d.addLast(6);
+        d.addLast(7);
+
+        assertThat(d.get(0)).isEqualTo(6);
+        assertThat(d.get(1)).isEqualTo(7);
+        assertThat(d.get(7)).isEqualTo(5);
+        assertThat(d.get(6)).isEqualTo(4);
+        assertThat(d.get(5)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Test the method isEmpty")
+    void testIsEmpty() {
+        Deque61B<String> d = new ArrayDeque61B<>();
+
+        assertThat(d.isEmpty()).isTrue();
+
+        d.addLast("123");
+
+        assertThat(d.isEmpty()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Test the method size")
+    void testSize() {
+        Deque61B<Double> d = new ArrayDeque61B<>();
+        d.addLast(6.75);
+        d.addLast(6.33);
+
+        assertThat(d.size()).isEqualTo(2);
+
+        d.addFirst(2.09);
+        d.addFirst(0.00);
+
+        assertThat(d.size()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("Test the method toList")
+    void testToList() {
+        Deque61B<Integer> d = new ArrayDeque61B<>();
+        d.addFirst(5);
+        d.addFirst(4);
+        d.addFirst(3);
+        d.addLast(6);
+        d.addLast(7);
+
+        assertThat(d.toList()).containsExactly(3, 4, 5, 6, 7).inOrder();
+    }
+
+    @Test
+    @DisplayName("Test the methods remove")
+    void testRemove() {
+        Deque61B<String> d = new ArrayDeque61B<>();
+        d.addFirst("world");
+        d.addFirst("hello");
+        d.addLast("foo");
+        String a = d.removeLast();
+
+        assertThat(d.toList()).containsExactly("hello", "world").inOrder();
+
+        d.addFirst("baz");
+        d.addFirst("bar");
+        String b = d.removeFirst();
+        String c = d.removeFirst();
+
+        assertThat(d.toList()).containsExactly("hello", "world").inOrder();
+        assertThat(List.of(a, b, c)).containsExactly("foo", "bar", "baz").inOrder();
+    }
 
 }
