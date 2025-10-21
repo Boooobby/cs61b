592c0a4624f75434fd63ecf704f811e52d1fbb68
Boooobby <bibobu1223@gmail.com>
2025-10-21 21:12:20 +0800

    proj3: nothing

--- proj3/src/core/Room.java
+++ proj3/src/core/Room.java
@@ -1,4 +1,23 @@
 package core;
 
 public class Room {
+
+    private Point bottomLeft;
+    private int width;
+    private int height;
+    private static final int minWidth = 4;
+    private static final int minHeight = 4;
+
+    public Room(int x, int y, int w, int h) {
+        bottomLeft = new Point(x, y);
+        width = w;
+        height = h;
+    }
+
+    public Room(int x, int y) {
+        this(x, y, minWidth, minHeight);
+    }
+
+
+
 }
--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,7 +1,13 @@
 package core;
 
+import tileengine.TETile;
+
 public class World {
 
-    // build your own world!
+    private TETile[][] world;
+    private static final int minWidth = 60;
+    private static final int minHeight = 60;
+    private int width;
+    private int height;
 
 }
