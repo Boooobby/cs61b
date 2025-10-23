cd1245d17dbe710ee864db45b4516a0c63092dd4
Boooobby <bibobu1223@gmail.com>
2025-10-23 14:28:04 +0800

    proj3: framework of BSP

--- proj3/src/core/BSP.java
+++ proj3/src/core/BSP.java
@@ -25,8 +25,20 @@ public class BSP {
         root = new Node(new Point(0, 0), world.length, world[0].length);
     }
 
-    private Node splitTheWorld() {
-        return null;
+    private Node splitTheWorld(Node node) {
+        if (isTooSmall(node)) {
+            createRoom();
+            return node;
+        }
+
+        if (node.width >= node.height) {
+            // split by width
+        } else {
+            // split by height
+        }
+
+        node = connectTheRoom(node);
+        return node;
     }
 
     private boolean isTooSmall(Node node) {
@@ -37,8 +49,8 @@ public class BSP {
 
     }
 
-    private void connectTheRoom() {
-
+    private Node connectTheRoom(Node node) {
+        return null;
     }
 
     public TETile[][] generateTheWorld() {
