c05d4f599400096146a495c56061e84acbc46ec5
Boooobby <bibobu1223@gmail.com>
2025-10-24 01:33:04 +0800

    proj3: code cleanup

--- proj3/src/core/Room.java
+++ proj3/src/core/Room.java
@@ -9,9 +9,10 @@ public class Room {
     private Point bottomLeft;
     private int width;
     private int height;
-    public static final int minWidth = 2;
-    public static final int minHeight = 2;
     private Random random;
+
+    public static final int minWidth = 3;
+    public static final int minHeight = 3;
 
     public Room(int x, int y, int w, int h, Random r) {
         bottomLeft = new Point(x, y);
