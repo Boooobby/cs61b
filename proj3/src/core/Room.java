e8053a0233202ae2538db20e857c7d4bf023e499
Boooobby <bibobu1223@gmail.com>
2025-10-23 12:30:52 +0800

    proj3: create Room class

--- proj3/src/core/Room.java
+++ proj3/src/core/Room.java
@@ -1,12 +1,16 @@
 package core;
 
+import utils.RandomUtils;
+
+import java.util.Random;
+
 public class Room {
 
     private Point bottomLeft;
     private int width;
     private int height;
-    private static final int minWidth = 4;
-    private static final int minHeight = 4;
+    private static final int minWidth = 2;
+    private static final int minHeight = 2;
 
     public Room(int x, int y, int w, int h) {
         bottomLeft = new Point(x, y);
@@ -18,6 +22,10 @@ public class Room {
         this(x, y, minWidth, minHeight);
     }
 
-
+    Point chooseAPoint() {
+        int x = RandomUtils.uniform(new Random(), bottomLeft.getX(), bottomLeft.getX() + width);
+        int y = RandomUtils.uniform(new Random(), bottomLeft.getY(), bottomLeft.getY() + height);
+        return new Point(x, y);
+    }
 
 }
