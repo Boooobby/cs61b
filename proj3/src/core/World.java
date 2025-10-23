44b862db5df715d9b42421d81c791b4a7f7127a8
Boooobby <bibobu1223@gmail.com>
2025-10-23 15:29:33 +0800

    Update World.java

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -2,6 +2,8 @@ package core;
 
 import tileengine.TETile;
 
+import java.util.Random;
+
 public class World {
 
     private TETile[][] world;
@@ -9,5 +11,13 @@ public class World {
     private static final int minHeight = 60;
     private int width;
     private int height;
+
+    private BSP bsp;
+    private Random random;
+    private long seed;
+
+    public World(long seed) {
+        
+    }
 
 }
