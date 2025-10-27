0084f2632e9de655fd6106dcf8cf5e54a13eb451
Boooobby <bibobu1223@gmail.com>
2025-10-27 11:29:40 +0800

    proj3: completed 3B 完结撒花~

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
 
         // build your own world!
-        int width = 30;
-        int height = 30;
+        int width = 40;
+        int height = 40;
 
         World w = new World(102002302L, width, height);
         w.generateWorld();
